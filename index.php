57df4e823d24e65ec5d48c523ba85e6c78a1b2fd
Justshad <shadex.raphael00@yahoo.com>
2023-01-27 12:04:29 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+// commencer par l'appel du controller
+// require(controllers/...)
+
+//Définition de la constante du titre de la page, que nous utilisons dans le head
+define("PAGE_TITLE", "Accueil")
+
+?>
+<?php include("assets/inc/headFront.php"); ?>
+<?php include("assets/inc/headerFront.php"); ?>
+    <main>
+   
+
+
+    </main>
+    <?php
+include("assets/inc/footerFront.php");
+?>
